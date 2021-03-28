dad9fae28b653fee32d91f2700a23baa142de7ea
Tamara Rodina <arktv88@gmail.com>
2021-03-28 19:26:01 +0300

    Second

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int account = 2_000_000_000;
+        int deposit = 500_000_000;
+        int total = deposit + account;
+        System.out.println("Total: " + total);
+    }
+}
